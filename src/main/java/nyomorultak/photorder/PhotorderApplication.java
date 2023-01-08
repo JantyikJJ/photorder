29a4a0e9abf476f15ef06c851119e1ebd6a4f9b1
ExModify <exmodify@gmail.com>
2023-01-08 12:09:46 +0100

    Initial commit

---
+++ src/main/java/nyomorultak/photorder/PhotorderApplication.java
@@ -0,0 +1,13 @@
+package nyomorultak.photorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nyomorultak/photorder/PhotorderApplicationTests.java
@@ -0,0 +1,13 @@
+package nyomorultak.photorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
