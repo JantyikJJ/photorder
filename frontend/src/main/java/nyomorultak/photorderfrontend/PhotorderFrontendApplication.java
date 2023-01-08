c34055d537e040a8f70c53acd826b16aaa9566f0
ExModify <exmodify@gmail.com>
2023-01-08 12:15:13 +0100

    Add frontend template and refactor code

---
+++ frontend/src/main/java/nyomorultak/photorderfrontend/PhotorderFrontendApplication.java
@@ -0,0 +1,13 @@
+package nyomorultak.photorderfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotorderFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotorderFrontendApplication.class, args);
+	}
+
+}
---
+++ frontend/src/test/java/nyomorultak/photorderfrontend/PhotorderFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package nyomorultak.photorderfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotorderFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
