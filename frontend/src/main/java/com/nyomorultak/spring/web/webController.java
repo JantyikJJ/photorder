8ca73856749f9c16c1f29f3ca246f6f56f907f2f
barnus0705 <barnakaskaroly@gmail.com>
2023-01-09 13:22:58 +0100

    Change frontend template.

---
+++ frontend/src/main/java/com/nyomorultak/spring/web/webController.java
@@ -0,0 +1,16 @@
+package com.gyak.spring.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class webController {
+
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
--- frontend/src/test/java/com/nyomorultak/spring/ApplicationTests.java
+++ frontend/src/test/java/com/nyomorultak/spring/ApplicationTests.java
@@ -1,13 +1,13 @@
-package nyomorultak.photorderfrontend;
+package com.gyak.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PhotorderFrontendApplicationTests {
+class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
